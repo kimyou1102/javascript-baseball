091fd643c5b39624ea231090042445da8317de25
kimyou1102 <yoosk5485@naver.com>
2022-11-27 19:16:57 +0900

    refactor: 3자리수를 입력하지 않으면 발생하는 예외를 추가한다

--- src/Validate.js
+++ src/Validate.js
@@ -1,12 +1,18 @@
 const Validate = {
-    validateInputNumbers(numbers) {
-        const set = new Set(numbers);
-        if(numbers.length !== set.size) {
-            throw new Error("중복된 값이 있습니다. 서로 다른 3자리의 수를 입력해주세요.")
-        }
-
-
+  validateInputNumbers(numbers) {
+    const set = new Set(numbers);
+    if (numbers.length !== set.size) {
+      throw new Error(
+        "중복된 값이 있습니다. 서로 다른 3자리의 수를 입력해주세요."
+      );
     }
-}
+
+    if (numbers.length !== 3) {
+      throw new Error(
+        "3자리 수를 입력해주세요."
+      );
+    }
+  },
+};
 
 module.exports = Validate;
