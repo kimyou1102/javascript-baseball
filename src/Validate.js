929efba27159e4494241bd804d5a7494c12baf91
kimyou1102 <yoosk5485@naver.com>
2022-12-12 18:22:06 +0900

    refactor: 숫자를 입력할 때 예외처리를 추가한다
    - 자릿수에 0이 포함되는 예외처리
    - 숫자가 아닌 다른값을 입력했을 때 예외처리

--- src/Validate.js
+++ src/Validate.js
@@ -2,6 +2,8 @@ const Validate = {
   Warning: {
     duplicateValue: "중복된 값이 있습니다. 서로 다른 3자리의 수를 입력해주세요.",
     length: "3자리 수를 입력해주세요.",
+    range: "각 자릿수에 1이상의 숫자를 입력해주세요.",
+    onlyNumber: "숫자만 입력할 수 있습니다. 각 자릿수에 1에서9사이의 값을 입력해주세요."
   },
 
   validateInputNumbers(numbers) {
@@ -14,6 +16,16 @@ const Validate = {
     if (numbers.length !== DIGITS) {
       throw new Error(this.Warning.length);
     }
+
+    if(numbers.includes(0)) {
+      throw new Error(this.Warning.range);
+    }
+
+    numbers.split('').forEach((number) => {
+      if(isNaN(Number(number)) || number === " ") {        
+        throw new Error(this.Warning.onlyNumber);
+      }
+    })
   },
 };
 
