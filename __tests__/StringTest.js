6fc9d5adb797b7a0c76efe777f58f55dcd17d5df
kimyou1102 <yoosk5485@naver.com>
2022-12-13 17:09:45 +0900

    test: 테스트코드를 작성한다
    - 사용자 입력값과 컴퓨터 값 비교하는 함수 테스트
    - 입력받은 숫자에 대한 예외처리 테스트

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,13 +1,42 @@
 const Referee = require('../src/Referee');
-const OutputView = require('../src/OutputView');
+const Validate = require('../src/Validate');
 
 describe("테스트", () => {
-  test("사용자 입력값과 컴퓨터 값 비교 테스트", () => {
-    expect(new Referee().compare([7,1,3], '173')).toEqual({ball: 1, strike: 2});
+  test("사용자 입력값과 컴퓨터 값 비교 테스트 - 낫싱", () => {
+    expect(new Referee().compare([1,3,5], '246')).toEqual({ball: 0, strike: 0, nothing:true});
   });
 
-  test("힌트 출력 테스트", () => {
-
-    expect(OutputView.printHint({ ball: 0, strike: 2 })).toEqual('2스트라이크');
+  test("사용자 입력값과 컴퓨터 값 비교 테스트 - 3스트라이크", () => {
+    expect(new Referee().compare([1,3,5], '135')).toEqual({ball: 0, strike: 3, nothing:false});
   });
+
+  test("사용자 입력값과 컴퓨터 값 비교 테스트 - 2볼 1스트라이크", () => {
+    expect(new Referee().compare([1,3,5], '153')).toEqual({ball: 2, strike: 1, nothing:false});
+  });
+
+  test.each([["12"], ["1234"], ["1"]])("3자리 숫자를 입력하지 않은 경우에 대한 예외처리", (input) => {
+    expect(() => {
+      Validate.validateInputNumbers(input)
+    }).toThrow();
+  });
+
+  test.each([["122"], ["112"], ["333"]])("중복된 숫자를 입력한 경우에 대한 예외처리", (input) => {
+    expect(() => {
+      Validate.validateInputNumbers(input)
+    }).toThrow();
+  });
+
+  test.each([["012"], ["000"], ["190"]])("입력한 숫자의 범위가 틀린 경우에 대한 예외처리", (input) => {
+    expect(() => {
+      Validate.validateInputNumbers(input)
+    }).toThrow();
+  });
+
+  test.each([["2ㅇp"], ["12 "], ["   "]])("숫자가 아닌 값을 입력한 경우에 대한 예외처리", (input) => {
+    expect(() => {
+      Validate.validateInputNumbers(input)
+    }).toThrow();
+  });
+
+
 });
